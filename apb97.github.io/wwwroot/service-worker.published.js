b45878c068434ce0008fb7ca11e37954a0d3ad73
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-05-03 18:00:26 +0200

    Update service-worker.

--- apb97.github.io/wwwroot/service-worker.published.js
+++ apb97.github.io/wwwroot/service-worker.published.js
@@ -45,7 +45,7 @@ async function onFetch(event) {
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
         const shouldServeIndexHtml = event.request.mode === 'navigate'
             && !manifestUrlList.some(url => url === event.request.url)
-            && !/\/Web(.*|\/)$/.test(event.request.url);
+            && !/\/WebSudoku(.*|\/)$/.test(event.request.url);
 
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
         const cache = await caches.open(cacheName);
