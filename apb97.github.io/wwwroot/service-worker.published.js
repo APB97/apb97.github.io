6d04dea32fc06f77821d0d4d13651bcab121c2bd
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-04 09:53:37 +0200

    Update service-worker

--- apb97.github.io/wwwroot/service-worker.published.js
+++ apb97.github.io/wwwroot/service-worker.published.js
@@ -52,7 +52,7 @@ async function onFetch(event) {
         cachedResponse = await cache.match(request);
 
         if (/\/WebSudoku\/(sudoku|rules)/.test(event.request.url)) {
-            return fetch('/WebSudoku/', { mode: 'navigate', redirect: 'follow' });
+            return fetch(event.request.url);
         }
         else if (/\/service-worker.js$/.test(event.request.url)) {
             return fetch('/service-worker.js');
