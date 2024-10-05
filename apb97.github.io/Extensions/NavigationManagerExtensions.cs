7e75c797b4fe05122cceb31ac7630bc448dcd6e5
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-05 15:28:53 +0200

    Refactor code from Index.razor into extension method.
    Update layout of Index Page and its' AboutMe section.
    
    Make link colors consistent between Projects and Challenges sections.

---
+++ apb97.github.io/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace apb97.github.io.Extensions
+{
+    public static class NavigationManagerExtensions
+    {
+        private const string DestinationKey = "destination";
+
+        public static async Task NavigateToStoredDestinationIfAnyAsync(this NavigationManager navigation, IJSRuntime js)
+        {
+            var destination = await js.InvokeAsync<string>("sessionStorage.getItem", DestinationKey);
+            if (string.IsNullOrEmpty(destination)) return;
+            await js.InvokeVoidAsync("sessionStorage.removeItem", DestinationKey);
+            navigation.NavigateTo(destination[1..]);
+        }
+    }
+}
