55f849cc968f11cf248785b5f499918046bf8cea
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-06 19:52:24 +0200

    Introduce Hidden and HiddenForScreenWidth components (with JS Interop).
    Introduce AdjustedAnchorHeading component for fixing headings with anchor tags
    to navigate to correct positions.
    
    Update MainLayout

---
+++ apb97.github.io/Services/WindowDimensions.cs
@@ -0,0 +1,8 @@
+﻿namespace apb97.github.io.Services
+{
+    public class WindowDimensions
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
---
+++ apb97.github.io/Shared/HiddenForScreenWidth.razor.js
@@ -0,0 +1,16 @@
+﻿export function addListener(dotnetHelper, methodName) {
+    window.addEventListener('load', () => {
+        dotnetHelper.invokeMethodAsync(methodName, window.innerWidth);
+    });
+
+    window.addEventListener('resize', () => {
+        dotnetHelper.invokeMethodAsync(methodName, window.innerWidth);
+    });
+}
+
+export function getDimensions() {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+};
