ffcfb3bb812982a74c1568c2294d937320ebb3ff
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-16 22:01:16 +0200

    Update AdjustedAnchorHeading to work around navigation to items with certain id attribute

---
+++ apb97.github.io/Shared/AdjustedAnchorHeading.razor.js
@@ -0,0 +1,4 @@
+﻿export function activateCurrentLink(url) {
+    document.querySelectorAll('a.nav-link.active')?.forEach(n => n.classList.remove('active'));
+    document.querySelector(`a.nav-link[href='${url.replace(/\//, '')}']`)?.classList?.add('active');
+}
