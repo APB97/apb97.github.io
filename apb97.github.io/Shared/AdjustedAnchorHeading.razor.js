d813a2cc818338d5b855cf5d642e54a8e7be0e6d
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-17 17:47:12 +0200

    Fix navigation to elements by id

--- apb97.github.io/Shared/AdjustedAnchorHeading.razor.js
+++ apb97.github.io/Shared/AdjustedAnchorHeading.razor.js
@@ -1,4 +1,11 @@
-﻿export function activateCurrentLink(url) {
-    document.querySelectorAll('a.nav-link.active')?.forEach(n => n.classList.remove('active'));
-    document.querySelector(`a.nav-link[href='${url.replace(/\//, '')}']`)?.classList?.add('active');
+﻿export function activateCurrentLink(url, hash) {
+    var h = document.querySelector(hash);
+    h?.scrollIntoView();
+    document.querySelectorAll(`a.nav-link.active:not([href='${url.replace(document.location.origin, '').replace(/\//, '')}'])`)?.forEach(n => n.classList.remove('active'));
+    const currentNavLink = document.querySelector(`a.nav-link[href='${url.replace(document.location.origin, '').replace(/\//, '')}']`);
+    currentNavLink?.classList?.add('active');
+    currentNavLink?.click();
+    if (h == null && currentNavLink != null) {
+        setTimeout(r => activateCurrentLink(url, hash), 200);
+    }
 }
