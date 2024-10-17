db048236fd563760e7df46d01658444590780965
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-17 17:59:38 +0200

    Introduce NavigationByIdFix component

---
+++ apb97.github.io/Shared/NavigationByIdFix.razor.js
@@ -0,0 +1,13 @@
+﻿export function activateCurrentLink(url, hash) {
+    const h = document.querySelector(hash);
+    h?.scrollIntoView();
+    const href = url.replace(document.location.origin, '').replace(/\//, '');
+    document.querySelectorAll(`a.nav-link.active:not([href='${href}'])`)
+        ?.forEach(n => n.classList.remove('active'));
+    const currentNavLink = document.querySelector(`a.nav-link[href='${href}']`);
+    currentNavLink?.classList?.add('active');
+    currentNavLink?.click();
+    if (h == null && currentNavLink != null) {
+        setTimeout(r => activateCurrentLink(url, hash), 200);
+    }
+}
