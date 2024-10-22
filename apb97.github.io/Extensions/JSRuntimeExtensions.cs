1f420daa55b86fb6ea28a0c525927c607d44f0d0
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-22 15:47:00 +0200

    Add ImportAsync extension method for IJSRuntime

---
+++ apb97.github.io/Extensions/JSRuntimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+
+namespace apb97.github.io.Extensions;
+
+public static class JSRuntimeExtensions
+{
+    private const string ImportFunction = "import";
+
+    public static ValueTask<IJSObjectReference> ImportAsync(this IJSRuntime js, string path)
+    {
+        return js.InvokeAsync<IJSObjectReference>(ImportFunction, path);
+    }
+}
--- apb97.github.io/Extensions/NavigationManagerExtensions.cs
+++ apb97.github.io/Extensions/NavigationManagerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using apb97.github.io.JSInterop;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
 namespace apb97.github.io.Extensions;
@@ -9,9 +10,10 @@ public static class NavigationManagerExtensions
 
     public static async Task NavigateToStoredDestinationIfAnyAsync(this NavigationManager navigation, IJSRuntime js)
     {
-        var destination = await js.InvokeAsync<string>("sessionStorage.getItem", DestinationKey);
+        await using var utiltiesModule = await js.ImportAsync(JSModules.UtilitiesModule);
+        var destination = await utiltiesModule.GetSessionSettingAsync<string>(DestinationKey);
         if (string.IsNullOrEmpty(destination)) return;
-        await js.InvokeVoidAsync("sessionStorage.removeItem", DestinationKey);
+        await utiltiesModule.RemoveSessionSettingAsync(DestinationKey);
         navigation.NavigateTo(destination[1..]);
     }
 }
